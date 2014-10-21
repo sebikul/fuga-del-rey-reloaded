b142d9a451c1d788acf1adaa5e9cf76368f0aafc
sebikul <sebikul@gmail.com>
2014-10-21 18:19:55 -0300

    La interfaz es mas facil de usar

--- src/gui/GraphicalBoard.java
+++ src/gui/GraphicalBoard.java
@@ -2,7 +2,6 @@ package gui;
 
 import java.awt.Color;
 import java.awt.Component;
-import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -10,10 +9,8 @@ import java.awt.event.MouseEvent;
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTable;
-import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
 import javax.swing.table.AbstractTableModel;
@@ -38,16 +35,10 @@ public class GraphicalBoard {
 	private TableModel tableModel;
 	private Game game;
 	private JLabel lblJugador;
-	private JPanel panel_1;
 	private JPanel panel;
-	private JLabel lblNewLabel;
-	private JLabel lblNewLabel_1;
-	private JTextField txtFOrigen;
-	private JTextField txtFDestino;
-	private JLabel lblColumnaOrigen;
-	private JLabel lblColumnaDestino;
-	private JTextField txtCOrigen;
-	private JTextField txtCDestino;
+	private JLabel lblError;
+	private JPanel panel_2;
+	private JPanel panel_3;
 
 	/**
 	 * Create the application.
@@ -92,51 +83,30 @@ public class GraphicalBoard {
 		panel = new JPanel();
 
 		frame.getContentPane().add(panel);
+		panel.setLayout(new GridLayout(0, 1, 0, 0));
+
+		panel_2 = new JPanel();
+		panel.add(panel_2);
 
 		JLabel lblTurnoDe = new JLabel("Turno de: ");
+		panel_2.add(lblTurnoDe);
 		lblTurnoDe.setHorizontalAlignment(SwingConstants.TRAILING);
-		panel.add(lblTurnoDe);
 
 		lblJugador = new JLabel("JUGADOR");
-		panel.add(lblJugador);
+		panel_2.add(lblJugador);
+
 		panel.setFocusTraversalPolicy(new FocusTraversalOnArray(
 				new Component[] { lblTurnoDe }));
 
-		panel_1 = new JPanel();
-		frame.getContentPane().add(panel_1);
-		panel_1.setLayout(new GridLayout(2, 4, 0, 0));
+		panel_3 = new JPanel();
+		panel.add(panel_3);
 
-		lblNewLabel = new JLabel("Fila origen");
-		panel_1.add(lblNewLabel);
-
-		txtFOrigen = new JTextField();
-		panel_1.add(txtFOrigen);
-		txtFOrigen.setColumns(10);
-
-		lblColumnaOrigen = new JLabel("Columna origen");
-		panel_1.add(lblColumnaOrigen);
-
-		txtCOrigen = new JTextField();
-		panel_1.add(txtCOrigen);
-		txtCOrigen.setColumns(10);
-
-		lblNewLabel_1 = new JLabel("Fila destino");
-		panel_1.add(lblNewLabel_1);
-
-		txtFDestino = new JTextField();
-		txtFDestino.setColumns(10);
-		panel_1.add(txtFDestino);
-
-		lblColumnaDestino = new JLabel("Columna destino");
-		panel_1.add(lblColumnaDestino);
-
-		txtCDestino = new JTextField();
-		panel_1.add(txtCDestino);
-		txtCDestino.setColumns(10);
+		lblError = new JLabel("");
+		panel_3.add(lblError);
 
 		table.addMouseListener(new MouseAdapter() {
 
-			private boolean isOrigenSet = false;
+			private Punto origen, destino;
 
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -144,55 +114,44 @@ public class GraphicalBoard {
 				int fila = table.rowAtPoint(e.getPoint());
 				int columna = table.columnAtPoint(e.getPoint());
 
-				if (isOrigenSet == false) {
-					txtCOrigen.setText("" + columna);
-					txtFOrigen.setText("" + fila);
-					isOrigenSet = true;
+				if (origen == null) {
+
+					origen = new Punto(fila, columna);
+
 				} else {
-					txtCDestino.setText("" + columna);
-					txtFDestino.setText("" + fila);
-					isOrigenSet = false;
+
+					destino = new Punto(fila, columna);
 
 					try {
-						int filaOrigen = Integer.parseInt(txtFOrigen.getText());
-						int columnaOrigen = Integer.parseInt(txtCOrigen
-								.getText());
-						int filaDestino = Integer.parseInt(txtFDestino
-								.getText());
-						int columnaDestino = Integer.parseInt(txtCDestino
-								.getText());
 
-						Jugador result = game.mover(new Punto(filaOrigen,
-								columnaOrigen), new Punto(filaDestino,
-								columnaDestino));
+						Jugador result = game.mover(origen, destino);
 
 						if (result != null) {
-							JOptionPane.showMessageDialog(null, "El jugador "
-									+ result + " ha ganado", "Hay un ganador!",
-									JOptionPane.INFORMATION_MESSAGE);
+
+							lblError.setText("El jugador " + result
+									+ " ha ganado");
+
 						}
 
-					} catch (NumberFormatException
-							| BoardPointOutOfBoundsException e1) {
-						JOptionPane.showMessageDialog(null,
-								"Las coordenadas ingresadas son invalidas",
-								"Error de formato", JOptionPane.ERROR_MESSAGE);
+					} catch (BoardPointOutOfBoundsException e1) {
+
+						lblError.setText("Las coordenadas ingresadas son invalidas");
+
 						return;
 
 					} catch (MovimientoBloqueadoException e1) {
 
-						JOptionPane.showMessageDialog(null,
-								"El movimiento esta bloqueado.",
-								"Error de movimiento",
-								JOptionPane.ERROR_MESSAGE);
+						lblError.setText("El movimiento esta bloqueado.");
+
 						return;
 
 					} catch (MovimientoInvalidoException e1) {
-						JOptionPane.showMessageDialog(null,
-								"El movimiento es invalido.",
-								"Error de movimiento",
-								JOptionPane.ERROR_MESSAGE);
+
+						lblError.setText("El movimiento es invalido.");
+
 						return;
+					} finally {
+						origen = destino = null;
 					}
 
 					((AbstractTableModel) table.getModel())
@@ -206,16 +165,14 @@ public class GraphicalBoard {
 			}
 		});
 
-		table.setRowHeight(400 / table.getModel().getRowCount());
+		table.setRowHeight(450 / table.getModel().getRowCount());
 
 		frame.setVisible(true);
 	}
 
 	private void limpiarCoordenadas() {
-		txtCDestino.setText("");
-		txtCOrigen.setText("");
-		txtFDestino.setText("");
-		txtFOrigen.setText("");
+
+		lblError.setText("");
 	}
 
 	@SuppressWarnings("serial")
